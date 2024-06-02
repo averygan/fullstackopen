e98517c3678cf0dff0fb83256e3db80c63c682d6
averygan <avery@Averys-MacBook-Air.local>
2024-06-02 15:10:27 +0800

    completed ex 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -14,19 +14,12 @@ const Part = (content) => {
   )
 }
 
-const Content = () => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+const Content = ({part1, part2, part3}) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
     </div>
   )
 }
@@ -41,15 +34,24 @@ const Total = (total) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header name={course} />
-      <Content />
-      <Total sum={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
