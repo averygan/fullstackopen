c8d2d9645be456e881f21c48dedd15b474e909d5
averygan <averygan19@gmail.com>
2024-07-02 15:09:58 +0800

    completed ex 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,7 +5,6 @@ import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 const App = () => {
-  // const [persons, setPersons] = useState([]) 
   const [persons, setPersons] = useState([])
   
   const hook = () => {
@@ -44,7 +43,12 @@ const App = () => {
         name: newName,
         number: newNumber
       }
-      setPersons(persons.concat(personObject))
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          console.log(response.data)
+          setPersons(persons.concat(response.data))
+        })
     }
     else alert(`${newName} is already added to phonebook`)
     setNewName('')
