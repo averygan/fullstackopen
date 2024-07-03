82acf7db9bb4d84ffa2f266a5bea0e6c639bd597
averygan <averygan19@gmail.com>
2024-07-03 15:40:29 +0800

    completed ex 2.19 - added show button for displayResults

--- part2/dataforcountries/src/components/DisplayResults.jsx
+++ part2/dataforcountries/src/components/DisplayResults.jsx
@@ -1,9 +1,25 @@
+import DisplayCountry from './DisplayCountry'
+import { useState } from 'react'
+
 const DisplayResults = ({countries}) => {
+    const [selectedCountry, setSelectedCountry] = useState(null)
+
+    const toggleShow = (country) => {
+        if (selectedCountry === country)
+            setSelectedCountry(null)
+        else
+            setSelectedCountry(country)
+    }
+
     return (
         <div>
-            {countries.map((countries, index) => 
-                <p key={index}>{countries}</p>
-            )}
+            {countries.map((country, index) => (
+                <div key={index}>
+                    <p>{country}</p>
+                    <button onClick={() => toggleShow(country)}>show</button>
+                    {selectedCountry === country && <DisplayCountry country={country} />}
+                </div>
+            ))}
         </div>
     )
 }
--- part2/dataforcountries/src/components/Results.jsx
+++ part2/dataforcountries/src/components/Results.jsx
@@ -5,10 +5,10 @@ const Results = ({countries, filter}) => {
     const filtered = filter === '' ? countries : countries.filter(country => country.toLowerCase().includes(filter.toLowerCase()))
     console.log(filtered)
     if (filtered.length === 1)
-        return <DisplayCountry country={filtered} />
+        return <DisplayCountry country={filtered[0]} />
     if (filtered.length > 1 && filtered.length <= 10)
         return <DisplayResults countries={filtered} />
     return null;
-}
+} 
 
 export default Results
